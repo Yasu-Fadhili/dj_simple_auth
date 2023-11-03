d0b719cdac77b1ec2f3ebc14f286bfc773c7a3d1
Yasu Fadhili <yasufadhili@gmail.com>
2023-11-03 07:52:08 +0300

    Setup urls

---
+++ authentication/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from .views import (
+    UserRegistrationView, UserLoginView, UserLogoutView, CustomAuthToken
+)
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path("obtain-token/", CustomAuthToken.as_view())
+]
+
+
+
